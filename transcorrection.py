70a71378ccbe246d711f6d06606f64760de253cb
Athar Sayed <sayedathar242@gmail.com>
2025-05-29 21:21:23 +0530

    Added transcript correction layer for handling incorrect words

---
+++ transcorrection.py
@@ -0,0 +1,7 @@
+import language_tool_python
+
+def correct_transcript(text):
+    tool = language_tool_python.LanguageTool('en-US')
+    matches = tool.check(text)
+    corrected_text = language_tool_python.utils.correct(text, matches)
+    return corrected_text
